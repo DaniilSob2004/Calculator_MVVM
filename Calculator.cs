4110acfb93c171d6c4441807864062bd4fd497fe
Daniil <Soboliev.danik@gmail.com>
2023-07-03 21:39:27 +0300

    Create project

---
+++ Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Lesson5
+{
+    public class Calculator
+    {
+        public int RightOperand { get; set; }
+        public int LeftOperand { get; set; }
+        public string Operator { get; set; }
+
+        public int Total
+        {
+            get
+            {
+                switch (Operator)
+                {
+                    case "+": return LeftOperand + RightOperand;
+                    case "-": return LeftOperand - RightOperand;
+                    case "*": return LeftOperand * RightOperand;
+                    case "/":
+                        if (RightOperand != 0)
+                            return LeftOperand / RightOperand;
+                        return -1;
+                }
+                return -1;
+            }
+        }
+    }
+}
---
+++ ViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Lesson5
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private Calculator calc;
+
+        public ViewModel()
+        {
+            calc = new Calculator() { LeftOperand = 0, RightOperand = 0, Operator = "+" };
+        }
+
+        public string LOperand
+        {
+            get => calc.LeftOperand.ToString();
+            set
+            {
+                try
+                {
+                    calc.LeftOperand = int.Parse(value);
+                    OnPropertyChanged("LOperand");
+                    Total = calc.Total;
+                }
+                catch (FormatException) { }
+            }
+        }
+        public string ROperand
+        {
+            get => calc.RightOperand.ToString();
+            set
+            {
+                try
+                {
+                    calc.RightOperand = int.Parse(value);
+                    OnPropertyChanged("ROperand");
+                    Total = calc.Total;
+                }
+                catch (FormatException) { }
+            }
+        }
+        public string Operation
+        {
+            get => calc.Operator;
+            set
+            {
+                calc.Operator = value.Substring(value.IndexOf(" ") + 1, 1);
+                OnPropertyChanged("Operation");
+                Total = calc.Total;
+            }
+        }
+        public int Total
+        {
+            get => calc.Total;
+            set => OnPropertyChanged("Total");
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                PropertyChanged(this, e);
+            }
+        }
+    }
+}
